4d18b94c8ee997ee312c7d94d4a4681822886e89
Takatoshi Ono <takatoshi.ono@gmail.com>
2014-09-16 20:11:14 +0900

    Add alias to ssh command

--- zshrc.d/aliases.zsh
+++ zshrc.d/aliases.zsh
@@ -1,2 +1,3 @@
 alias vi='env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/Vim "$@"'
 alias vim='env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/Vim "$@"'
+alias ssh='TERM=xterm-256color ssh'
