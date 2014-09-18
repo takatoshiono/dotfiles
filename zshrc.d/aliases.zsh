bd4839e809e2d1449df100f05ed384635a68c4c1
Takatoshi Ono <takatoshi.ono@gmail.com>
2014-09-18 17:49:41 +0900

    Add mvim to alias

--- zshrc.d/aliases.zsh
+++ zshrc.d/aliases.zsh
@@ -1,3 +1,4 @@
 alias vi='env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/Vim "$@"'
 alias vim='env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/Vim "$@"'
+alias mvim='env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/mvim "$@"'
 alias ssh='TERM=xterm-256color ssh'
