d21909bb4a8790b26d78c4d11be0a3849a8c2ceb
Takatoshi Ono <takatoshi.ono@gmail.com>
2017-08-21 18:05:22 +0900

    I dont use MacVim now

--- zshrc.d/aliases.zsh
+++ zshrc.d/aliases.zsh
@@ -1,3 +1,1 @@
-alias vim='env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/Vim "$@"'
-alias mvim='env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/mvim "$@"'
 alias ssh='TERM=xterm-256color ssh'
