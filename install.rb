91618f844d8f78d7c179b53110fe74c1da7f63af
Takatoshi Ono <takatoshi.ono@gmail.com>
2018-07-20 18:16:03 +0900

    remove oh-my-zsh submodule

--- install.rb
+++ install.rb
@@ -24,7 +24,7 @@ class DotfileInstaller
 
   def execute
     %w(
-      oh-my-zsh vim ctags editorconfig gitconfig gitignore gvimrc gemrc my.cnf tmux.conf vimrc zshrc
+      vim ctags editorconfig gitconfig gitignore gvimrc gemrc my.cnf tmux.conf vimrc zshrc
     ).each do |resource|
       copy resource
     end
